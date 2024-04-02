6b76e4936387ea0ca82ca16e73b22d98dc688790
SaiPrasad-22 <saisarikond22@gmail.com>
2024-04-02 19:02:53 +0530

    committ neww

--- Mar21.js
+++ Mar21.js
@@ -9,41 +9,81 @@ function addTask() {
     card.className = "card";
     card.innerHTML = "Add Task"
     card.setAttribute("contenteditable", true);
+    card.setAttribute("draggable", true)
     todo.append(card);
     card.focus();
 
-    // card.addEventListener("blur", (eventDetails)=>{
+
+    card.addEventListener("dragstart", (eventDetails)=>{
+        let dragCard = eventDetails.target;
+        dragCard.style.opacity = 0.5;
+    })
+
+    card.addEventListener("dragend", (eventDetails)=>{
+        let dragCard = eventDetails.target;
+        dragCard.style.opacity = 1;
+    })
+
+   
+    card.addEventListener("dragover", (eventDetails)=>{
+        eventDetails.preventDefault();
+    })
+    card.addEventListener("dragenter", (eventDetails)=>{
+        eventDetails.preventDefault();
+    })
+    card.addEventListener("drop", (eventDetails)=>{
+        eventDetails.preventDefault();
+    })
+
+
+
+
+
+
+    // card.addEventListener("blur", blurFunction);
+
+    // function blurFunction(eventDetails) {
     //     let blurredCard = eventDetails.target;
-    //     if(blurredCard.innerText.trim() == ""){
+    //     if (blurredCard.innerHTML.trim() == "") {
     //         blurredCard.remove();
     //     }
+    // }
 
-    card.addEventListener("blur", blurFunction);
+    // card.addEventListener("click", emptyText);
 
-    function blurFunction(eventDetails) {
-        let blurredCard = eventDetails.target;
-        if (blurredCard.innerHTML.trim() == "") {
-            blurredCard.remove();
-        }
-    }
+    // function emptyText(eventDetails) {
+    //     let clickedCard = eventDetails.target;
+    //     if (clickedCard.innerText == "Add Task") {
+    //         clickedCard.innerText = "";
+    //     }
+    // }
 
-    card.addEventListener("click", emptyText);
+    // // SELECTOR -->
 
-    function emptyText(eventDetails) {
-        let clickedCard = eventDetails.target;
-        if (clickedCard.innerText == "Add Task") {
-            clickedCard.innerText = "";
-        }
-    }
+    // let selector = document.createElement("select");
+    // selector.innerHTML =
+    //     `   <option value="todo">ToDo</option>
+    //     <option value="progress">Progress</option>
+    //     <option value="done">Done</option>         
+    // `
+    // card.append(selector);
+
+    // let selectedId = {
+    //     todo: "todo",
+    //     progress: "progress",
+    //     done: "done"
+    // }
+
+    // selector.addEventListener("change", (eventDetails) => {
+    //     console.log(eventDetails.target.value);
+    //     console.log("column moved-", selectedId[eventDetails.target.value]);
+    //     let columnId = selectedId[eventDetails.target.value];
+    //     let selectedContainer = document.querySelector(`#${columnId}`)
+    //     console.log(selectedContainer);
+    //     selectedContainer.append(card);
+    // })
 
 
-    let selector = document.createElement("select");
-    selector.innerHTML =
-    `   <option value="todo">ToDo</option>
-        <option value="progress">Progress</option>
-        <option value="done">Done</option>         
-    `
-    card.append(selector);
 
 
 }
